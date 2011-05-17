18e7b2f81ff40350db773ac4fce3f9c270b41b2b
David Sveningsson <ext@sidvind.com>
2011-05-17 16:46:53 +0000

    c linkage
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/libcap_utils/trunk@1793 8459abcf-8237-4014-9f1e-057f68ddaf13

--- caputils/utils.h
+++ caputils/utils.h
@@ -1,9 +1,18 @@
 #ifndef CAPUTILS_UTILS_H
 #define CAPUTILS_UTILS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * Text representation of error code.
  */
 const char* caputils_error_string(int code);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* CAPUTILS_UTILS_H */
