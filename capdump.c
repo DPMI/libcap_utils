514345e38e4b44343172ab31d1c9e638511bd8b1
David Sveningsson <ext@sidvind.com>
2011-08-03 13:38:31 +0000

    starting a proper implementation of capdump
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/libcap_utils/trunk@2002 8459abcf-8237-4014-9f1e-057f68ddaf13

--- capdump.c
+++ capdump.c
@@ -2,7 +2,7 @@
 #include "config.h"
 #endif /* HAVE_CONFIG_H */
 
-#include <caputils/stream.h>
+#include <caputils/caputils.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,117 +11,67 @@
 #include <getopt.h>
 #include <errno.h>
 
-int clone_stream(struct stream* dst, struct stream* src, unsigned long long* matches){
-  cap_head* cp;
-  size_t len = sizeof(struct cap_header);
-
-  *matches = 0;
-  while ( 1 ){
-    long ret = stream_read(src, &cp, NULL);
-    if ( ret == EAGAIN ){
-      continue;
-    } else if ( ret != 0 ){
-      break;
-    }
-
-    (*matches)++;
-    if( !(stream_write(dst, (char*)cp, cp->caplen + len)) ) {
-      fprintf(stderr, "Problems writing data to file!");
-    }
-  }
-
-  return 0;
-}
-
-int display_stream(struct stream* src, const struct filter* filter, unsigned long long* matches){
-  cap_head* cp;
-  time_t time;
-  long ret;
-
-  *matches = 0;
-  while ( 1 ) {
-    ret = stream_read(src, (char**)&cp, filter);
-    if ( ret == EAGAIN ){
-      continue;
-    } else if ( ret != 0 ){
-      break;
-    }
-
-    if ( cp->caplen == 0 ){
-      fprintf(stderr, "caplen is zero, will skip this packet but most likely the stream got out-of-sync and will crash later.\n");
-      continue;
-    }
-    
-    (*matches)++;
-    time = (time_t)cp->ts.tv_sec;
-    
-    fprintf(stdout, "[%llu]:%.4s:%.8s:", *matches, cp->nic, cp->mampid);
-    if( args.cDate == 0 ) {
-      fprintf(stdout, "%u.", cp->ts.tv_sec);
-    } else {
-      static char timeStr[25];
-      struct tm tm = *gmtime(&time);
-      strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", &tm);
-      fprintf(stdout, "%s.", timeStr);
-    }
-
-    fprintf(stdout, "%012lu:LINK(%d):CAPLEN(%d):", cp->ts.tv_psec, cp->len, cp->caplen);
-
-    struct frame_t frame;
-    if ( classify_packet(cp, &frame) == 0 ){
-      print_frame(stdout, &frame, args.print_content);
-    } else {
-      fprintf(stderr, "Got an unknown packet.\n");
-      if(cp->len<60) {
-	fprintf(stdout, "%02x:%02x:%02x:%02x:%02x:%02x -> %02x:%02x:%02x:%02x:%02x:%02x [%04x]:", 
-		frame.eth->h_source[0],frame.eth->h_source[1],frame.eth->h_source[2],frame.eth->h_source[3],frame.eth->h_source[4],frame.eth->h_source[5],
-		frame.eth->h_dest[0],  frame.eth->h_dest[1],  frame.eth->h_dest[2],  frame.eth->h_dest[3],  frame.eth->h_dest[4],  frame.eth->h_dest[5], 
-		ntohs(frame.eth->h_proto));
-      }
-      fprintf(stdout, "\n");
-      continue;
-    }
-
-    if ( args.max_pkts > 0 && *matches + 1 > args.max_pkts) {
-      /* Read enough pkts lets break. */
-      printf("read enought packages\n");
-      break;
-    }
-  }
-
-  if ( ret == -1 ){ /* EOF, TCP shutdown, etc */
-    fprintf(stderr, "Finished\n");
-    return 0;
-  } 
-
-  fprintf(stderr, "readpost() returned 0x%08lx: %s\n", ret, caputils_error_string(ret));
-  return 0;  
-}
-
+static const char* program_name = NULL;
 static struct option long_options[]= {
-  {"output",  1, 0, 'o'},
-  {"packets", 1, 0, 'p'},
-  {"help", 0, 0, 'h'},
-  {0, 0, 0, 0}
+  {"output",  required_argument, 0, 'o'},
+  {"packets", required_argument, 0, 'p'},
+  {"iface",   required_argument, 0, 'i'},
+  {"comment", required_argument, 0, 'c'},
+  {"help",    no_argument,       0, 'h'},
+  {0, 0, 0, 0} /* sentinel */
 };
+
+static void show_usage(){
+  printf("capdump-" VERSION "\n");
+  printf("(C) 2011 David Sveningsson <david.sveningsson@bth.se>\n");
+  printf("Usage: %s [OPTIONS] STREAM\n", program_name);
+  printf("  -o, --output=FILE    Save output in capfile. [default=stdout]\n"
+	 "  -i, --iface          For ethernet-based streams, this is the interface to listen\n"
+	 "                       on. For other streams it is ignored.\n"
+	 "  -p, --packets=INT    Stop capture after INT packages.\n"
+	 "  -c, --comment        Set stream comment.\n"	 
+	 "  -h, --help           This text.\n");
+  printf("\n");
+  printf("Streams can be specified in the following formats:\n");
+  printf("  - NN:NN:NN:NN:NN:NN  Listen to ethernet multicast stream.\n"
+	 "  - tcp://IP[:PORT]    Listen to TCP unicast.\n"
+	 "  - udp://IP[:PORT]    Listen to UDP broadcast.\n"
+	 "  - FILENAME           Open capfile for reading.\n");
+}
 
 int main(int argc, char **argv){
   extern int optind, opterr, optopt;
+  int op, option_index = -1;
+  program_name = strrchr(argv[0], '/') + 1;
 
-  int option_index;
+  char* comment = "capdump-" VERSION " stream";
+  char* iface = NULL;
+  destination_t input;
+  destination_t output;
+  destination_aton(&output, "/dev/stdout", DEST_CAPFILE, DEST_LOCAL);
 
-  char* outFilename=0;
-  struct stream* src;
-  struct stream* dst;
-  
-  if ( argc < 2 ){
-    show_usage();
-    exit(0);
-  }
-  
-  while ( (op = getopt_long(argc, argv, "ho:p:", long_options, &option_index)) != -1 )
+  long max_packets = -1;
+
+  while ( (op = getopt_long(argc, argv, "ho:p:c:i:", long_options, &option_index)) != -1 ){
     switch (op){
-    case 0: /* long opt */
+    case 0:   /* long opt */
+    case '?': /* unknown opt */
+      break;
+
+    case 'o':
+      destination_aton(&output, optarg, 0, DEST_GUESS);
+      break;
+
+    case 'p':
+      max_packets = atoi(optarg);
+      break;
+
+    case 'c':
+      comment = optarg;
+      break;
+
+    case 'i':
+      iface = optarg;
       break;
       
     case 'h':
@@ -130,51 +80,86 @@ int main(int argc, char **argv){
       break;
       
     default:
-      printf ("?? getopt returned character code 0%o ??\n", op);
+      if ( option_index >= 0 ){
+	fprintf(stderr, "flag --%s declared but not handled\n", long_options[option_index].name);
+      } else {
+	fprintf(stderr, "flag -%c declared but not handled\n", op);
+      }
+      abort();
     }
+    option_index = -1;
+  }
 
-  printf("Opening stream %s\n", filename);
-  destination_t src;
-  destination_aton(&src, filename, streamType, DEST_LOCAL);
+  if ( optind == argc ){
+    show_usage();
+    exit(0);
+  }
 
-  if( (ret=openstream(&inStream, &src, nic, portnumber)) != 0 ) {
-    fprintf(stderr, "openstream failed with code 0x%08X: %s\n", ret, caputils_error_string(ret));
+  struct stream* src;
+  struct stream* dst;
+  int ret;
+
+  /* parse stream address */
+  if ( (ret=destination_aton(&input, argv[optind], DEST_NONE, DEST_GUESS)) != 0 ){
+    fprintf(stderr, "Failed to parse stream address: %s\n", caputils_error_string(ret));
     return 1;
   }
 
-  if(capOutfile==1) {
-    fprintf(stderr, "Creating FILE!\n.");
-    destination_t dst;
-    destination_aton(&dst, outFilename, DEST_CAPFILE, DEST_LOCAL);
-    createstream(&outStream, &dst, NULL, stream_get_mampid(inStream), stream_get_comment(inStream));
-    fprintf(stderr, "OK.\n");
+  /* ensure iface has been configured for ethernet streams */
+  if ( input.type == DEST_ETHERNET && !iface ){
+    fprintf(stderr, "Ethernet streams require --iface\n");
+    return 1;
+  }
+
+  /* open input stream */
+  static char* type[4] = {"file", "ethernet", "udp", "tcp"};
+  fprintf(stderr, "Opening %s stream: %s\n", type[input.type], destination_ntoa(&input));
+  if ( (ret=openstream(&src, &input, iface, 0)) != 0 ) {
+    fprintf(stderr, "openstream() failed with code 0x%08X: %s\n", ret, caputils_error_string(ret));
+    return 1;
+  }
+
+  /* open output stream */
+  if ( (ret=createstream(&dst, &output, NULL, stream_get_mampid(src), comment)) != 0 ){
+    fprintf(stderr, "createstream() failed with code 0x%08X: %s\n", ret, caputils_error_string(ret));
+    return 1;
   }
 
   struct file_version version;
-  stream_get_version(inStream, &version);
+  stream_get_version(src, &version);
 
 //output fileheader
   fprintf(stderr, "ver: %d.%d id: %s \n comments: %s\n",
 	  version.major, 
 	  version.minor, 
-	  stream_get_mampid(inStream), 
-	  stream_get_comment(inStream));
+	  stream_get_mampid(src), 
+	  stream_get_comment(src));
 
-  fprintf(stderr, "myFilter.index = %u \n", myfilter.index);
-  fprintf(stderr, "----------------------------\n");
+  cap_head* cp;
+  size_t len = sizeof(struct cap_header);
 
-  if ( capOutfile == 1 ){
-    ret = clone_stream(outStream, inStream, &myfilter, &pktCount);
-  } else {
-    ret = display_stream(inStream, &myfilter, &pktCount);
+  long matches = 0;
+  while ( 1 ){
+    long ret = stream_read(src, &cp, NULL);
+    if ( ret == EAGAIN ){
+      continue;
+    } else if ( ret != 0 ){
+      break;
+    }
+
+    matches++;
+    if( !(stream_write(dst, (char*)cp, cp->caplen + len)) ) {
+      fprintf(stderr, "Problems writing data to file!");
+    }
+
+    if ( matches >= max_packets ){
+      break;
+    }
   }
-  
-  closestream(inStream);
 
-  if(capOutfile==1) {
-    closestream(outStream);
-  }
+  closestream(src);
+  closestream(dst);
 
-  fprintf(stderr, "There was a total of %lld pkts that matched the filter.\n", pktCount);
+  fprintf(stderr, "There was a total of %ld pkts that matched the filter.\n", matches);
   return 0;
 }
