a2c3f6a2db83874cc38cc3284c4def045aec849f
David Sveningsson <ext@sidvind.com>
2012-01-12 18:54:34 +0000

    reindenting
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/libcap_utils/trunk@2350 8459abcf-8237-4014-9f1e-057f68ddaf13

--- address.c
+++ address.c
@@ -39,7 +39,7 @@ static char* strtok2(char* str, char* delim){
 static void homogenize_eth_addr(char* buf){
 	const size_t len = strlen(buf);
 
-  /* convert dash to colon */
+	/* convert dash to colon */
 	for ( int i = 0; i < 17; i++ ){
 		if ( buf[i] == '-' ) buf[i] = ':';
 	}
@@ -120,148 +120,148 @@ static void homogenize_eth_addr(char* buf){
 }
 
 int stream_addr_aton(stream_addr_t* dst, const char* src, enum AddressType type, int flags){
-  char buf[48];          /* larger than max, just in case user provides large */
-  strncpy(buf, src, 48); /* input, will bail out later on bad data. */
+	char buf[48];          /* larger than max, just in case user provides large */
+	strncpy(buf, src, 48); /* input, will bail out later on bad data. */
 
-  memset(dst->buffer, 0, 26);
-  dst->_type = htons(type);
-  dst->_flags = htons(flags);
+	memset(dst->buffer, 0, 26);
+	dst->_type = htons(type);
+	dst->_flags = htons(flags);
 
-  switch( type ){
-  case STREAM_ADDR_GUESS:
-  {
-    char* delim = strstr(buf, "://");
+	switch( type ){
+	case STREAM_ADDR_GUESS:
+		{
+			char* delim = strstr(buf, "://");
 
-    /* check if prefix is set */
-    if ( delim ){
-      *delim = 0;
-      const char* prefix = buf;
-      const char* addr = delim + 3;
+			/* check if prefix is set */
+			if ( delim ){
+				*delim = 0;
+				const char* prefix = buf;
+				const char* addr = delim + 3;
 	    
-      if ( strcasecmp("tcp", prefix) == 0 ){
-	      return stream_addr_aton(dst, addr, STREAM_ADDR_TCP, flags);
-      } else if ( strcasecmp("udp", prefix) == 0 ){
-	      return stream_addr_aton(dst, addr, STREAM_ADDR_UDP, flags);
-      } else if ( strcasecmp("eth", prefix) == 0 ){
-	      return stream_addr_aton(dst, addr, STREAM_ADDR_ETHERNET, flags);
-      } else if ( strcasecmp("file", prefix) == 0 ){
-	      return stream_addr_aton(dst, src+7, STREAM_ADDR_CAPFILE, flags | STREAM_ADDR_LOCAL);
-      }
+				if ( strcasecmp("tcp", prefix) == 0 ){
+					return stream_addr_aton(dst, addr, STREAM_ADDR_TCP, flags);
+				} else if ( strcasecmp("udp", prefix) == 0 ){
+					return stream_addr_aton(dst, addr, STREAM_ADDR_UDP, flags);
+				} else if ( strcasecmp("eth", prefix) == 0 ){
+					return stream_addr_aton(dst, addr, STREAM_ADDR_ETHERNET, flags);
+				} else if ( strcasecmp("file", prefix) == 0 ){
+					return stream_addr_aton(dst, src+7, STREAM_ADDR_CAPFILE, flags | STREAM_ADDR_LOCAL);
+				}
       
-      return EINVAL;
-    }
+				return EINVAL;
+			}
     
-    /* try ethernet */
-    if ( stream_addr_aton(dst, src, STREAM_ADDR_ETHERNET, flags) == 0 ){
-	    return 0;
-    }
+			/* try ethernet */
+			if ( stream_addr_aton(dst, src, STREAM_ADDR_ETHERNET, flags) == 0 ){
+				return 0;
+			}
     
-    /* last option: parse as local filename */
-    return stream_addr_aton(dst, src, STREAM_ADDR_CAPFILE, flags | STREAM_ADDR_LOCAL);
-  }
+			/* last option: parse as local filename */
+			return stream_addr_aton(dst, src, STREAM_ADDR_CAPFILE, flags | STREAM_ADDR_LOCAL);
+		}
 
-  case STREAM_ADDR_TCP: // TCP
-  case STREAM_ADDR_UDP: // UDP
-    // DESTADDR is ipaddress:port
-    {
-      char* ip = buf;
-      strncpy(buf, src, 48);
+	case STREAM_ADDR_TCP: // TCP
+	case STREAM_ADDR_UDP: // UDP
+		// DESTADDR is ipaddress:port
+		{
+			char* ip = buf;
+			strncpy(buf, src, 48);
 
-      dst->in_port = htons(0x0810); /* default port */
+			dst->in_port = htons(0x0810); /* default port */
 
-      char* separator = strchr(buf, ':');
-      if( separator ) {
-	*separator = 0;
-	dst->in_port = htons(atoi(separator+1));
-      }
+			char* separator = strchr(buf, ':');
+			if( separator ) {
+				*separator = 0;
+				dst->in_port = htons(atoi(separator+1));
+			}
 
-      dst->in_addr.s_addr = inet_addr(ip);
+			dst->in_addr.s_addr = inet_addr(ip);
 
-      if ( dst->in_addr.s_addr == INADDR_NONE ){
-	return EINVAL;
-      }
-    }
-    break;
+			if ( dst->in_addr.s_addr == INADDR_NONE ){
+				return EINVAL;
+			}
+		}
+		break;
 
-  case STREAM_ADDR_ETHERNET: // Ethernet
-    homogenize_eth_addr(buf);
+	case STREAM_ADDR_ETHERNET: // Ethernet
+		homogenize_eth_addr(buf);
     
-    if ( !eth_aton(&dst->ether_addr, buf) ){
-      return EINVAL;
-    }
-    break;
+		if ( !eth_aton(&dst->ether_addr, buf) ){
+			return EINVAL;
+		}
+		break;
 
-  case STREAM_ADDR_CAPFILE: // File
-    if ( flags & STREAM_ADDR_LOCAL ){
-      dst->local_filename = src;
-    } else {
-      strncpy(dst->filename, src, 22);
-      dst->filename[21] = 0; /* force null-terminator */
-    }
-    break;
-  }
+	case STREAM_ADDR_CAPFILE: // File
+		if ( flags & STREAM_ADDR_LOCAL ){
+			dst->local_filename = src;
+		} else {
+			strncpy(dst->filename, src, 22);
+			dst->filename[21] = 0; /* force null-terminator */
+		}
+		break;
+	}
 
-  return 0;
+	return 0;
 }
 
 int stream_addr_str(stream_addr_t* dst, const char* src, int flags){
-  dst->_type = htons(STREAM_ADDR_CAPFILE);
-  dst->_flags = htons(STREAM_ADDR_LOCAL|flags);
-  dst->local_filename = src;
-  return 0;
+	dst->_type = htons(STREAM_ADDR_CAPFILE);
+	dst->_flags = htons(STREAM_ADDR_LOCAL|flags);
+	dst->local_filename = src;
+	return 0;
 }
 
 const char* stream_addr_ntoa(const stream_addr_t* src){
-  static char buf[1024];
-  return stream_addr_ntoa_r(src, buf, 1024);
+	static char buf[1024];
+	return stream_addr_ntoa_r(src, buf, 1024);
 }
 
 const char* stream_addr_ntoa_r(const stream_addr_t* src, char* buf, size_t bytes){
-  int __attribute__((unused)) written = 0;
+	int __attribute__((unused)) written = 0;
 
-  switch(stream_addr_type(src)){
-  case STREAM_ADDR_GUESS:
-    abort();
+	switch(stream_addr_type(src)){
+	case STREAM_ADDR_GUESS:
+		abort();
 
-  case STREAM_ADDR_TCP:
-  case STREAM_ADDR_UDP:
-    written = snprintf(buf, bytes, "%s://%s:%d", stream_addr_type(src) == STREAM_ADDR_UDP ? "udp" : "tcp", inet_ntoa(src->in_addr), ntohs(src->in_port));
-    break;
-  case STREAM_ADDR_ETHERNET:
-    hexdump_address_r(&src->ether_addr, buf);
-    break;
-  case STREAM_ADDR_CAPFILE:
-	  if ( stream_addr_have_flag(src, STREAM_ADDR_LOCAL) ){
-      strncpy(buf, src->local_filename, bytes);
-    } else {
-      strncpy(buf, src->filename, bytes);
-    }
-    buf[bytes-1] = 0; /* force null-terminator */
-    break;
-  }
+	case STREAM_ADDR_TCP:
+	case STREAM_ADDR_UDP:
+		written = snprintf(buf, bytes, "%s://%s:%d", stream_addr_type(src) == STREAM_ADDR_UDP ? "udp" : "tcp", inet_ntoa(src->in_addr), ntohs(src->in_port));
+		break;
+	case STREAM_ADDR_ETHERNET:
+		hexdump_address_r(&src->ether_addr, buf);
+		break;
+	case STREAM_ADDR_CAPFILE:
+		if ( stream_addr_have_flag(src, STREAM_ADDR_LOCAL) ){
+			strncpy(buf, src->local_filename, bytes);
+		} else {
+			strncpy(buf, src->filename, bytes);
+		}
+		buf[bytes-1] = 0; /* force null-terminator */
+		break;
+	}
 
-  assert(written < bytes);
-  return buf;
+	assert(written < bytes);
+	return buf;
 }
 
 enum AddressType stream_addr_type(const stream_addr_t* addr){
-  return (enum AddressType)ntohs(addr->_type);
+	return (enum AddressType)ntohs(addr->_type);
 }
 
 int stream_addr_flags(const stream_addr_t* addr){
-  return ntohs(addr->_flags);
+	return ntohs(addr->_flags);
 }
 
 int stream_addr_have_flag(const stream_addr_t* addr, enum AddressFlags flag){
-  return ntohs(addr->_flags) & flag;
+	return ntohs(addr->_flags) & flag;
 }
 
 void stream_addr_reset(stream_addr_t* addr){
-  memset(addr, 0, sizeof(stream_addr_t));
+	memset(addr, 0, sizeof(stream_addr_t));
 }
 
 int stream_addr_is_set(const stream_addr_t* addr){
-  /* potentially another address type might have the first bytes as a zero so
-   * the type is also checked */
-  return addr->_type != 0 || addr->filename[0] != 0;
+	/* potentially another address type might have the first bytes as a zero so
+	 * the type is also checked */
+	return addr->_type != 0 || addr->filename[0] != 0;
 }
