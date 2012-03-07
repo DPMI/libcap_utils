ad1fef20532b3a53f440964c23b40dcecadb87c1
David Sveningsson <ext@sidvind.com>
2012-03-07 21:22:22 +0100

    Adding headers and putting overview in function.

--- capinfo.c
+++ capinfo.c
@@ -13,17 +13,24 @@
 #include <netinet/ip.h>
 #include <netdb.h>
 
-static int packet_flag = 0;
-
 #define STPBRIDGES 0x0026
 #define CDPVTP 0x016E
+
+static stream_t st = NULL;
+static long int packets = 0;
+static uint64_t bytes = 0;
+static long arp = 0;
+static long stp = 0;
+static long cdpvtp = 0;
+static long other = 0;
+static long ipproto[UINT8_MAX] = {0,}; /* protocol is defined as 1 octet */
+static timepico first, last;
 
 static void show_usage(void){
 	printf("capinfo  caputils-" CAPUTILS_VERSION "\n");
 	printf("(c) 2011 David Sveningsson\n\n");
 	printf("Open a capstream and show information about it.\n");
 	printf("Usage: capinfo [OPTIONS] FILENAME..\n\n");
-	printf("      --packets              Show how many packets it contain.\n");
 	printf("  -h, --help                 Show this help.\n");
 }
 
@@ -40,7 +47,7 @@ static void format_bytes(char* dst, size_t size, uint64_t bytes){
 	snprintf(dst, size, "%.1f %s (%"PRIu64" bytes)", tmp, prefix[n], bytes);
 }
 
-void format_seconds(char* dst, size_t size, timepico first, timepico last){
+static void format_seconds(char* dst, size_t size, timepico first, timepico last){
 	const timepico time_diff = timepico_sub(last, first);
 	uint64_t hseconds = time_diff.tv_sec * 10 + time_diff.tv_psec / (PICODIVIDER / 10);
 
@@ -52,8 +59,32 @@ void format_seconds(char* dst, size_t size, timepico first, timepico last){
 	snprintf(dst, size, "%02d:%02d:%02.1f", h, m, (float)s/10);
 }
 
+static void print_overview(){
+	const char* mampid = stream_get_mampid(st);
+	const char* comment = stream_get_comment(st);
+
+	printf("Overview\n"
+	       "--------\n");
+	printf("     mpid: %s\n", mampid != 0 ? mampid : "(unset)");
+	printf("  comment: %s\n", comment ? comment : "(unset)");
+
+	char byte_str[128];
+	char first_str[128];
+	char last_str[128];
+	char sec_str[128];
+	const timepico time_diff = timepico_sub(last, first);
+	uint64_t hseconds = time_diff.tv_sec * 10 + time_diff.tv_psec / (PICODIVIDER / 10);
+	timepico_to_string(&first, first_str, 128, "%F %T");
+	timepico_to_string(&last,  last_str,  128, "%F %T");
+	format_bytes(byte_str, 128, bytes);
+	format_seconds(sec_str, 128, first, last);
+	printf(" captured: %s to %s\n", first_str, last_str);
+	printf(" duration: %s (%.1f seconds)\n", sec_str, (float)hseconds/10);
+	printf("  packets: %ld\n", packets);
+	printf("    bytes: %s\n", byte_str);
+}
+
 static int show_info(const char* filename){
-	stream_t st;
 	stream_addr_t addr;
 	stream_addr_str(&addr, filename, 0);
 	long ret = 0;
@@ -63,25 +94,7 @@ static int show_info(const char* filename){
 		return ret;
 	}
 
-	struct file_version version;
-	const char* mampid = stream_get_mampid(st);
-	const char* comment = stream_get_comment(st);
-	stream_get_version(st, &version);
-
-	printf("%s: caputils %d.%d stream\n", filename, version.major, version.minor);
-	printf("     mpid: %s\n", mampid != 0 ? mampid : "(unset)");
-	printf("  comment: %s\n", comment ? comment : "(unset)");
-
 	struct cap_header* cp;
-	long int packets = 0;
-	uint64_t bytes = 0;
-	long arp = 0;
-	long stp = 0;
-	long cdpvtp = 0;
-	long other = 0;
-	long ipproto[UINT8_MAX] = {0,}; /* protocol is defined as 1 octet */
-	timepico first, last;
-
 	while ( (ret=stream_read(st, &cp, NULL, NULL)) == 0 ){
 		packets++;
 
@@ -130,58 +143,53 @@ static int show_info(const char* filename){
 		fprintf(stderr, "stream_read() returned 0x%08lx: %s\n", ret, caputils_error_string(ret));
 	}
 
-	char byte_str[128];
-	char first_str[128];
-	char last_str[128];
-	char sec_str[128];
-	const timepico time_diff = timepico_sub(last, first);
-	uint64_t hseconds = time_diff.tv_sec * 10 + time_diff.tv_psec / (PICODIVIDER / 10);
-	timepico_to_string(&first, first_str, 128, "%F %T");
-	timepico_to_string(&last,  last_str,  128, "%F %T");
-	format_bytes(byte_str, 128, bytes);
-	format_seconds(sec_str, 128, first, last);
-	printf(" captured: %s to %s\n", first_str, last_str);
-	printf(" duration: %s (%.1f seconds)\n", sec_str, (float)hseconds/10);
-	printf("  packets: %ld\n", packets);
-	printf("    bytes: %s\n", byte_str);
+	/* write header */
+	struct file_version version;
+	stream_get_version(st, &version);
+	int n = printf("%s: caputils %d.%d stream\n", filename, version.major, version.minor);
+	while ( n-- ){ putchar('='); } puts("\n");
 
-	if ( packet_flag ){
-		long ipother = 0;
-		printf("    IP: ");
-		for ( int i = 0; i < UINT8_MAX; i++ ){
-			if ( ipproto[i] == 0 ){
-				continue;
-			}
+	/* overview */
+	print_overview();
 
-			struct protoent* protoent = getprotobynumber(i);
+	printf("\n"
+	       "Packet distribution\n"
+	       "-------------------\n");
 
-			if ( !protoent ){
-				ipother += ipproto[i];
-				continue;
-			}
+	long ipother = 0;
+	printf("       IP: ");
+	for ( int i = 0; i < UINT8_MAX; i++ ){
+		if ( ipproto[i] == 0 ){
+			continue;
+		}
 
-			printf("%s(%ld) ", protoent->p_name, ipproto[i]);
+		struct protoent* protoent = getprotobynumber(i);
+
+		if ( !protoent ){
+			ipother += ipproto[i];
+			continue;
 		}
-		if ( ipother > 0 ){
-			printf("other(%ld)", other);
-		}
-		printf("\n");
-		if ( arp > 0 ){
-			printf("    ARP: %ld\n", arp);
-		}
-		if ( stp > 0 ){
-			printf("    stpbridges: %ld\n", stp);
-		}
-		if ( cdpvtp > 0 ){
-			printf("    cdpvtp: %ld\n", cdpvtp);
-		}
-		if ( other > 0 ){
-			printf("    other: %ld\n", other);
-		}
+
+		printf("%s(%ld) ", protoent->p_name, ipproto[i]);
+	}
+	if ( ipother > 0 ){
+		printf("other(%ld)", other);
+	}
+	printf("\n");
+	if ( arp > 0 ){
+		printf("      ARP: %ld\n", arp);
+	}
+	if ( stp > 0 ){
+		printf("      STP: %ld\n", stp);
+	}
+	if ( cdpvtp > 0 ){
+		printf("   cdpvtp: %ld\n", cdpvtp);
+	}
+	if ( other > 0 ){
+		printf("    Other: %ld\n", other);
 	}
 
 	stream_close(st);
-
 	return 0;
 }
 
@@ -195,7 +203,6 @@ int main(int argc, char* argv[]){
 	/* parse arguments */
 	while (1){
 		static struct option long_options[] = {
-			{"packets", 0, &packet_flag,   1},
 			{"help",    0,            0, 'h'}
 		};
 
