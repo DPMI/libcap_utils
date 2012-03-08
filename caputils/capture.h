45813bf726e5111b9c1c01aadccb3929a1aa1a04
David Sveningsson <ext@sidvind.com>
2012-03-08 19:25:53 +0100

    Adding additional typedef for caphead

--- caputils/capture.h
+++ caputils/capture.h
@@ -18,9 +18,9 @@ struct ether_vlan_header{
 // Capture Header. This header is attached to each packet that we keep, i.e. it matched a filter.
 //
 //
-struct cap_header{ 
+struct cap_header{
   char nic[8];                          // Identifies the CI where the frame was caught
-  char mampid[8];                       // Identifies the MP where the frame was caught, 
+  char mampid[8];                       // Identifies the MP where the frame was caught,
   timepico ts;                          // Identifies when the frame was caught
   uint32_t len;                         // Identifies the lenght of the frame
   uint32_t caplen;                      // Identifies how much of the frame that we find here
@@ -32,10 +32,11 @@ struct cap_header{
     struct ether_vlan_header ethvlanhdr[0];
   };
 };
-typedef struct cap_header  cap_head;
+typedef struct cap_header cap_head;
+typedef struct cap_header* caphead_t;
 
 // Send Structure, used infront of each send data packet. The sequence number is indicates the number
-// of sent data packets. I.e. after a send packet this value is increased by one. 
+// of sent data packets. I.e. after a send packet this value is increased by one.
 struct sendhead {
   uint32_t sequencenr;                  // Sequence number.
   uint32_t nopkts;                      // How many packets are here.
