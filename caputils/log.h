ddebfeb72c50b07acad4b9ad5a71d58cd41b4dc9
David Sveningsson <ext@sidvind.com>
2012-03-19 16:46:14 +0100

    Reducing indentation.

--- caputils/log.h
+++ caputils/log.h
@@ -8,19 +8,19 @@ extern "C" {
 #include <stdio.h>
 #include <stdarg.h>
 
-  int vlogmsg(FILE* fp, const char* tag, const char* fmt, va_list ap);
-  int logmsg(FILE* fp, const char* tag, const char* fmt, ...) __attribute__ ((format (printf, 3, 4)));
+int vlogmsg(FILE* fp, const char* tag, const char* fmt, va_list ap);
+int logmsg(FILE* fp, const char* tag, const char* fmt, ...) __attribute__ ((format (printf, 3, 4)));
 
-  /**
-   * Dump the content of data as hexadecimal (and its ascii repr.)
-   */
-  void hexdump(FILE* fp, const char* data, size_t size);
+/**
+ * Dump the content of data as hexadecimal (and its ascii repr.)
+ */
+void hexdump(FILE* fp, const char* data, size_t size);
 
-  /**
-   * Dump the content of data as hexadecimal (and its ascii repr.) into a string.
-   * Memory should be freed with free.
-   */
-  char* hexdump_str(const char* data, size_t size);
+/**
+ * Dump the content of data as hexadecimal (and its ascii repr.) into a string.
+ * Memory should be freed with free.
+ */
+char* hexdump_str(const char* data, size_t size);
 
 #ifdef __cplusplus
 }
