a59f17dd62cb8359d1a97e548ecbf18090e75948
David Sveningsson <ext@sidvind.com>
2012-03-27 11:26:42 +0200

    Make is_marker public so consumers can use it.

--- caputils/marker.h
+++ caputils/marker.h
@@ -2,6 +2,7 @@
 #define CAPUTILS__MARKER_H
 
 #include <stdint.h>
+#include <caputils/capture.h>
 
 enum MarkerFlags {
 	MARKER_TERMINATE = (1<<0),
@@ -22,5 +23,16 @@ struct marker {
 
 	/* timeval depttime; */
 } __attribute__((packed));
+
+/**
+ * Test if packet is a marker packet.
+ * If port is non-zero an additional test is made to ensure the marker was sent
+ * on the given port. If zero the marker is searched for on any port. For
+ * reliable usage a port should always be given.
+ *
+ * It returns the port the marker was detected on or 0 if packet wasn't a
+ * marker. ptr is undefined if packet isn't a marker.
+ */
+int is_marker(struct cap_header* cp, struct marker* ptr, int port);
 
 #endif /* CAPUTILS__MARKER_H */
