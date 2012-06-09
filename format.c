f9f19bc6693705bb30feeacd28b092ee34b74955
David Sveningsson <ext@sidvind.com>
2012-06-10 01:29:07 +0200

    restructure of print_ethernet.

--- format.c
+++ format.c
@@ -166,55 +166,51 @@ static void print_eth(FILE* dst, const struct cap_header* cp, const struct ethhd
 
  begin:
 
-	if(h_proto<0x05DC){
-		fprintf(dst, "IEEE802.3 ");
-		fprintf(dst, "  %02x:%02x:%02x:%02x:%02x:%02x -> %02x:%02x:%02x:%02x:%02x:%02x ",
-		        eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5],
-		        eth->h_dest[0],  eth->h_dest[1],  eth->h_dest[2],  eth->h_dest[3],  eth->h_dest[4],  eth->h_dest[5]);
-		print_ieee8023(dst,(struct llc_pdu_sn*)payload);
-	} else {
-		switch ( h_proto ){
-		case ETHERTYPE_VLAN:
-			vlan_tci = ((uint16_t*)payload)[0];
-			h_proto = ntohs(((uint16_t*)payload)[0]);
-			payload = ((char*)eth) + sizeof(struct ethhdr);
-			fprintf(dst, "802.1Q vlan# %d: ", 0x0FFF&ntohs(vlan_tci));
-			goto begin;
+	switch ( h_proto ){
+	case ETHERTYPE_VLAN:
+		vlan_tci = ((uint16_t*)payload)[0];
+		h_proto = ntohs(((uint16_t*)payload)[0]);
+		payload = ((char*)eth) + sizeof(struct ethhdr);
+		fprintf(dst, "802.1Q vlan# %d: ", 0x0FFF&ntohs(vlan_tci));
+		goto begin;
 
-		case ETHERTYPE_IP:
-			fputs("IPv4", dst);
-			if ( flags >= FORMAT_LAYER_TRANSPORT ){
-				print_ipv4(dst, (struct ip*)payload);
-			}
-			break;
-
-		case ETHERTYPE_IPV6:
-			fputs("IPv6", dst);
-			break;
-
-		case ETHERTYPE_ARP:
-			print_arp(dst, cp, (const struct ether_arp*)payload);
-			break;
-
-		case 0x0810:
-			fprintf(dst, "MP packet");
-			break;
-
-		case STPBRIDGES:
-			fprintf(dst, "STP(0x%04x): (spanning-tree for bridges)", h_proto);
-			break;
-
-		case CDPVTP:
-			fprintf(dst, "CDP(0x%04x): (CISCO Discovery Protocol)", h_proto);
-			break;
-
-		default:
-			fprintf(dst, "Unknown ethernet protocol (0x%04x),  ", h_proto);
-			fputs(hexdump_address((const struct ether_addr*)eth->h_source), dst);
-			fputs(" -> ", dst);
-			fputs(hexdump_address((const struct ether_addr*)eth->h_dest), dst);
-			break;
+	case ETHERTYPE_IP:
+		fputs("IPv4", dst);
+		if ( flags >= FORMAT_LAYER_TRANSPORT ){
+			print_ipv4(dst, (struct ip*)payload);
 		}
+		break;
+
+	case ETHERTYPE_IPV6:
+		fputs("IPv6", dst);
+		break;
+
+	case ETHERTYPE_ARP:
+		print_arp(dst, cp, (const struct ether_arp*)payload);
+		break;
+
+	case 0x0810:
+		fprintf(dst, "MP packet");
+		break;
+
+	case STPBRIDGES:
+		fprintf(dst, "STP(0x%04x): (spanning-tree for bridges)", h_proto);
+		break;
+
+	case CDPVTP:
+		fprintf(dst, "CDP(0x%04x): (CISCO Discovery Protocol)", h_proto);
+		break;
+
+	default:
+		fprintf(dst, "IEEE802.3 [0x%04x] ", h_proto);
+		fputs(hexdump_address((const struct ether_addr*)eth->h_source), dst);
+		fputs(" -> ", dst);
+		fputs(hexdump_address((const struct ether_addr*)eth->h_dest), dst);
+		if(h_proto<0x05DC){
+			fputs(" ", dst);
+			print_ieee8023(dst,(struct llc_pdu_sn*)payload);
+		}
+		break;
 	}
 }
 
