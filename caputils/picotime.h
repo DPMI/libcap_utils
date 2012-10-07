688e2f192c425486e4c9e48bc1db5a4dbb798c99
David Sveningsson <ext@sidvind.com>
2012-10-07 15:56:10 +0200

    adding format specifier.

--- caputils/picotime.h
+++ caputils/picotime.h
@@ -41,12 +41,12 @@ int timepico_from_string(timepico* dst, const char* str);
  * Convert timepico to string (using strftime), psec ignored
  * @return dst or NULL if it fails.
  */
-const char* timepico_to_string_r(const timepico* src, char* dst, size_t bytes, const char* fmt);
+const char* timepico_to_string_r(const timepico* src, char* dst, size_t bytes, const char* fmt) __attribute__((format(strftime,4,0)));
 
 /**
  * Like timepico_to_string but using static memory.
  */
-const char* timepico_to_string(const timepico* src, const char* fmt);
+const char* timepico_to_string(const timepico* src, const char* fmt) __attribute__((format(strftime,2,0)));
 
 //compares two struct timepico (ts1<ts2=-1, ts1>ts2=1, ts1==ts2=0)
 int timecmp(const timepico *ts1, const timepico *ts2);
