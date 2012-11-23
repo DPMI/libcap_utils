5a94e08a4c0831c373220da27283cc084b125447
David Sveningsson <ext@sidvind.com>
2012-11-23 13:48:47 +0100

    [pcap2cap -I is redundant since -i will work as well.

--- pcap2cap.c
+++ pcap2cap.c
@@ -49,13 +49,13 @@ static int run = 1;
 static int quiet = 0;
 static size_t caplen = UINT16_MAX - sizeof(cap_head);
 
-static const char* shortopts = "c:o:m:i:I:l:qh";
+static const char* shortopts = "c:o:m:i:l:qh";
 static struct option longopts[] = {
 	{"comments",  required_argument, 0, 'c'},
 	{"output",    required_argument, 0, 'o'},
 	{"mpid",      required_argument, 0, 'm'},
 	{"interface", required_argument, 0, 'i'},
-	{"CI",        required_argument, 0, 'I'},
+	{"CI",        required_argument, 0, 'i'},
 	{"caplen",    required_argument, 0, 'l'},
 	{"quiet",     no_argument,       0, 'q'},
 	{"help",      no_argument,       0, 'h'},
@@ -73,9 +73,11 @@ static void show_usage(){
 	printf("  -m, --mpid=STRING          Set MP id, max 199 char. [default: hostname]\n");
 	printf("  -c, --comment=STRING       Add comment to header [default: none]\n");
 	printf("  -o, --output=FILENAME      Destination filename.\n");
-	printf("  -i, --interface=INTERFACE  Capture on live interface. (use \"any\" to capture\n"
-	       "                             on all interfaces)\n");
-	printf("  -I, --CI=INTERFACE         Set capture interface to INTERFACE [default: CONV]\n");
+	printf("  -i, --interface=INTERFACE  Capture Interface. If no input file is specified a\n"
+	       "                             pcap live capture will run on the specified\n"
+	       "                             interface. (use \"any\" to capture on all\n"
+	       "                             interfaces).\n"
+	       "      --CI=INTERFACE         Alias for --iface\n");
 	printf("      --caplen=INT           Set caplen. Default %zd bytes.\n", caplen);
 	printf("  -q, --quiet                Silent output, only errors is printed.\n");
 	printf("  -h, --help                 Show this help.\n");
@@ -190,13 +192,9 @@ int main (int argc, char **argv){
       strncpy(cp.mampid, optarg, 8);
       break;
 
-    case 'i':
+    case 'i': /* --iface */
       strncpy(cp.nic, optarg, 8);
       break;
-
-    case 'I': /* --ci */
-	    strncpy(cp.nic, optarg, 8);
-	    break;
 
     case 'l':
       caplen = atoi(optarg);
