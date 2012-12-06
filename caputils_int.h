3a3e47a2cb5b45689b2c22f636d8bf1098138736
David Sveningsson <ext@sidvind.com>
2012-12-06 13:57:23 +0100

    moving to stream.h

--- caputils_int.h
+++ caputils_int.h
@@ -51,35 +51,4 @@ enum {
   ERROR_LAST
 };
 
-int is_valid_version(struct file_header_t* fhptr);
-
-/**
- * Check and increment sequencenumber.
- * prints to stderr on mismatch.
- */
-void match_inc_seqnr(const struct stream* st, long unsigned int* restrict seq, const struct sendhead* restrict sh);
-
-int stream_udp_init(struct stream* st, const char* address, int port);
-int stream_tcp_init(struct stream* st, const char* address, int port);
-
-#ifdef HAVE_PFRING
-long stream_pfring_open(struct stream** stptr, const struct ether_addr* addr, const char* iface, size_t buffer_size);
-long stream_pfring_create(struct stream** stptr, const struct ether_addr* address, const char* iface, const char* mpid, const char* comment, int flags);
-long stream_pfring_add(struct stream* st, const struct ether_addr* addr);
-#else
-long stream_ethernet_open(struct stream** stptr, const struct ether_addr* address, const char* iface, size_t buffer_size);
-long stream_ethernet_create(struct stream** stptr, const struct ether_addr* address, const char* iface, const char* mpid, const char* comment, int flags);
-long stream_ethernet_add(struct stream* st, const struct ether_addr* addr);
-#endif
-
-/**
- * @param fp Optional, if null filename is used.
- */
-int stream_file_open(struct stream** stptr, FILE* fp, const char* filename, size_t buffer_size);
-
-/**
- * @param fp Optional, if null filename is used.
- */
-int stream_file_create(struct stream** stptr, FILE* fp, const char* filename, const char* mpid, const char* comment, int flags);
-
 #endif /* CAPUTILS_INT_H */
