f01e5a900e4dad32c502331dc401f0a991ca47e0
David Sveningsson <ext@sidvind.com>
2012-12-12 16:52:57 +0100

    Parse DNS response as well

--- format/dns.c
+++ format/dns.c
@@ -72,6 +72,20 @@ enum dns_class {
 	CLASS_MAX
 };
 
+enum response_code {
+	OK = 0,
+	FORMAT_ERROR,
+	SERVER_ERROR,
+	NAME_ERROR,
+	NOT_IMPLEMENTED,
+	REFUSED,
+	YXDomain,
+	YXRRSet,
+	NXRRSet,
+	NotAuth,
+	NotZone
+};
+
 static const char* dns_type_lut[TYPE_ANY+1] = {0,};
 static const char* dns_class_lut[CLASS_MAX] = {0,};
 static int initialized = 0;
@@ -96,28 +110,36 @@ static void dns_initialize(){
 	initialized = 1;
 }
 
-/**
- * Extract QNAME from a DNS question section.
- * @param dst Buffer when labels will be written.
- * @param size Number of bytes in dst buffer.
- * @param src Packet
- * @return New position in packet.
- */
-static const char* dns_qname(char* dst, size_t size, const char* src){
-	/* small hack to get "..." at the end */
-	size -= 3;
+static size_t copy_label(char** dst, size_t size, const char* src, size_t len){
+	if ( size == 0 ) return 0;
 
+	const int bytes = snprintf(*dst, size, "%.*s.", min(size, len), src);
+	*dst += min(bytes,size);
+
+	/* truncated */
+	if ( bytes > (int)size ){
+		sprintf(*dst-1, "...");
+		return size;
+	}
+
+	return bytes;
+}
+
+static const char* dns_name_int(char** dst, size_t* size, const char* src, const char* payload){
 	uint8_t len = *(const uint8_t*)(src++);
 	do {
-		if ( size > 0 ){
-			int bytes = snprintf(dst, size, "%.*s.", min(size,len), src);
-			dst  += min(bytes,size);
-			if ( bytes > (int)size ){ /* truncated */
-				bytes = size;
-				sprintf(dst-1, "...");
-			}
-			size -= bytes;
+
+		/* DNS uses a compression algorithm where if the first bits is 0xc0 it is a
+		 * reference to a previous label. It should therefore continue to read from
+		 * there until len is zero again. */
+		if ( (len & 0xc0) == 0xc0 ){
+			uint16_t offset = ((len & 0x3f) << 8) + *(const uint8_t*)(src++);
+			dns_name_int(dst, size, payload + offset, payload);
+			return src;
 		}
+
+		/* store the current label */
+		*size -= copy_label(dst, *size, src, len);
 		src += len;
 		len = *(const uint8_t*)(src++);
 	} while ( len > 0 );
@@ -125,12 +147,29 @@ static const char* dns_qname(char* dst, size_t size, const char* src){
 	return src;
 }
 
-static void print_query(FILE* fp, const struct dns_header* h, const char* ptr, unsigned int flags){
+/**
+ * Extract QNAME from a DNS question section.
+ * @param dst Buffer when labels will be written.
+ * @param size Number of bytes in dst buffer.
+ * @param src Current reading position in packet.
+ * @param payload Full packet.
+ * @return New position in packet.
+ */
+static const char* dns_name(char* dst, size_t size, const char* src, const char* payload){
+/* small hack to get "..." at the end */
+	if ( size != 0 ){
+		size -= 3;
+	}
+
+	return dns_name_int(&dst, &size, src, payload);
+}
+
+static void print_query(FILE* fp, const struct dns_header* h, const char* ptr, const char* payload, unsigned int flags){
 	fprintf(fp, " Standard query 0x%04x ", ntohs(h->id));
 	for ( unsigned int i = 0; i < ntohs(h->qdcount); i++ ){
 		if ( i > 1 ) fputs(", ", fp);
 		char qname[128];
-		ptr = dns_qname(qname, sizeof(qname), ptr);
+		ptr = dns_name(qname, sizeof(qname), ptr, payload);
 		uint16_t qtype  = ntohs(*(const uint16_t*)ptr); ptr += 2;
 		uint16_t qclass = ntohs(*(const uint16_t*)ptr); ptr += 2;
 
@@ -145,8 +184,78 @@ static void print_query(FILE* fp, const struct dns_header* h, const char* ptr, u
 	}
 }
 
-static void print_response(FILE* fp, const struct dns_header* h, const char* ptr, unsigned int flags){
+static void print_response(FILE* fp, const struct dns_header* h, const char* ptr, const char* packet, unsigned int flags){
 	fprintf(fp, " Standard query response 0x%04x ", ntohs(h->id));
+
+	/* discard question sections */
+	for ( unsigned int i = 0; i < ntohs(h->qdcount); i++ ){
+		ptr = dns_name(NULL, 0, ptr, packet);
+		ptr += 4; /* +4 to skip qtype and qclass */
+	}
+
+	if ( ntohs(h->ancount) == 0 ){
+		fprintf(fp, "(no answer section)");
+	}
+
+	/* process answer sections */
+	for ( unsigned int i = 0; i < ntohs(h->ancount); i++ ){
+		char name[128];
+		ptr = dns_name(name, sizeof(name), ptr, packet);
+
+		uint16_t type  = ntohs(*(const uint16_t*)ptr); ptr += 2;
+		uint16_t class = ntohs(*(const uint16_t*)ptr); ptr += 2;
+		uint32_t ttl   = ntohl(*(const uint32_t*)ptr); ptr += 4;
+		uint16_t rdlen = ntohs(*(const uint16_t*)ptr); ptr += 2;
+
+		/* hack to silence compiler */
+		ttl = ttl;
+
+		if ( type <= TYPE_ANY && dns_type_lut[type] ){
+			fprintf(fp, "%s ", dns_type_lut[type]);
+		} else {
+			fprintf(fp, "(%d) ", type);
+		}
+
+		if ( class == CLASS_IN ){
+			char buf[INET6_ADDRSTRLEN];
+			int tmp;
+
+			switch ( type ){
+			case TYPE_A:
+				fprintf(fp, "%s ", inet_ntop(AF_INET, ptr, buf, sizeof(buf)));
+				break;
+
+			case TYPE_AAAA:
+				fprintf(fp, "%s ", inet_ntop(AF_INET6, ptr, buf, sizeof(buf)));
+				break;
+
+			case TYPE_MX:
+				/* get priority field */
+				tmp = ntohs(*(const uint16_t*)ptr);
+				fprintf(fp, "%d ", tmp);
+				ptr += 2;
+				rdlen -= 2;
+				/* fall through */
+
+			case TYPE_NS:
+			case TYPE_CNAME:
+			case TYPE_SOA:
+			case TYPE_PTR:
+			case TYPE_TXT:
+			case TYPE_SPF:
+				dns_name(name, sizeof(name), ptr, packet);
+				fprintf(fp, " %s ", name);
+				break;
+
+			default:
+				break;
+			}
+		} else {
+			fprintf(fp, "%s ", dns_class_lut[class]);
+		}
+
+		ptr += rdlen;
+	}
 }
 
 void print_dns(FILE* fp, const struct cap_header* cp, const char* payload, size_t size, unsigned int flags){
@@ -175,6 +284,53 @@ void print_dns(FILE* fp, const struct cap_header* cp, const char* payload, size_
 		return;
 	}
 
+	switch ( (enum response_code)h.rcode ){
+	case OK:
+		break;
+
+	case FORMAT_ERROR:
+		fputs(" Format error", fp);
+		return;
+
+	case SERVER_ERROR:
+		fputs(" Server failure", fp);
+		return;
+
+	case NAME_ERROR:
+		fputs(" No such name", fp);
+		return;
+
+	case NOT_IMPLEMENTED:
+		fputs(" Not implemented", fp);
+		return;
+
+	case REFUSED:
+		fputs(" Refused", fp);
+		return;
+
+	case YXDomain:
+		fputs(" Name Exists when it should not", fp);
+		return;
+
+	case YXRRSet:
+		fputs(" RR Set Exists when it should not", fp);
+		return;
+
+	case NXRRSet:
+		fputs(" RR Set that should exist does not", fp);
+		return;
+
+	case NotAuth:
+		fputs(" Not Authoritative", fp);
+		return;
+
+	case NotZone:
+		fputs(" Name not contained in zone", fp);
+		return;
+
+	default:
+		fprintf(fp, " rcode: %d", h.rcode);
+	}
 
 	if ( bytes <= size - sizeof(struct dns_header) ){
 		fputs(" [Packet size limited during capture]", fp);
@@ -185,9 +341,9 @@ void print_dns(FILE* fp, const struct cap_header* cp, const char* payload, size_
 	switch ( h.opcode ){
 	case QUERY: /* standard query */
 		if ( h.qr == 0 ){
-			print_query(fp, &h, ptr, flags);
+			print_query(fp, &h, ptr, payload, flags);
 		} else {
-			print_response(fp, &h, ptr, flags);
+			print_response(fp, &h, ptr, payload, flags);
 		}
 		break;
 
