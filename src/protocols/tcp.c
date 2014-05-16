0b1aa91fffc3e46237475bda3e39afedf98fca99
EldinMalkoc <eldin.malkoc@gmail.com>
2014-05-16 12:45:02 +0200

    -

--- src/protocols/tcp.c
+++ src/protocols/tcp.c
@@ -195,10 +195,6 @@ static enum caputils_protocol_type next_payload(struct header_chunk* header, con
 
 static void tcp_format(FILE* fp, const struct header_chunk* header, const char* ptr, unsigned int flags){
 	fputs(": TCP", fp);
-	if ( limited_caplen(header->cp, ptr, sizeof(struct tcphdr)) ){
-		fputs(" [Packet size limited during capture]", fp);
-		return;
-	}
 
 	const struct tcphdr* tcp = (const struct tcphdr*)ptr;
 	const size_t header_size = 4*tcp->doff;
