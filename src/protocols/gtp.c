acb5edd0a7fa48407af1a180b11d9aca04e7a536
EldinMalkoc <eldin.malkoc@gmail.com>
2014-08-25 14:40:28 +0200

    Changed from gtp_header from union --> struct,

--- src/protocols/gtp.c
+++ src/protocols/gtp.c
@@ -25,15 +25,15 @@
 #include <endian.h>
 
 
-union gtp_header {
-	struct {
+struct gtp_header {
+
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-// 		uint8_t nextheader   : 8;
-// 		uint8_t pdunr        : 8;
-// 		uint16_t sequencenr   : 16;
-// 		uint32_t teid	      : 32;
-// 		uint16_t total	      : 16;
-// 		uint8_t message      : 8;
+		uint8_t nextheader   : 8;
+		uint8_t pdunr        : 8;
+		uint16_t sequencenr   : 16;
+		uint32_t teid	      : 32;
+		uint16_t total	      : 16;
+		uint8_t message      : 8;
 		uint8_t npdu	     : 1;
 		uint8_t seqflag      : 1;
 		uint8_t extension    : 1;
@@ -47,29 +47,22 @@ union gtp_header {
 		uint8_t extension    : 1;
 		uint8_t seqflag      : 1;
 		uint8_t npdu	     : 1;
-// 		uint8_t message      : 8;
-// 		uint16_t total	      : 16;
-// 		uint32_t teid	      : 32;
-// 		uint16_t sequencenr   : 16;
-// 		uint8_t pdunr        : 8;
-// 		uint8_t nextheader   : 8;
+		uint8_t message      : 8;
+		uint16_t total	      : 16;
+		uint32_t teid	      : 32;
+		uint16_t sequencenr   : 16;
+		uint8_t pdunr        : 8;
+		uint8_t nextheader   : 8;
 		
 #endif /* __BYTE_ORDER */
 
-		uint8_t nextheader   : 8;
-		uint8_t pdunr        : 8; 
-		uint16_t sequencenr   : 16;
-		uint32_t teid	      : 32;
-		uint16_t total	      : 16;
-		uint8_t message      : 8;	
-		
-	};
-	uint32_t val;
+	
+	
 } __attribute__((packed));
 
 
 static enum caputils_protocol_type gtp_header_next_payload(struct header_chunk* header, const char* ptr, const char** out){
-	const union gtp_header gtp = {.val = ntohl(*(const uint32_t*)ptr)};
+	const struct gtp_header* gtp = (const struct gtp_header*)ptr; 
 	const char* payload = ptr + 8;//+ sizeof(union gtp_header);
 	*out = payload;
 
@@ -83,32 +76,32 @@ static enum caputils_protocol_type gtp_header_next_payload(struct header_chunk* 
 		return PROTOCOL_IPV4;
 	}
 
-// 	/* detect IPv6 */
-// 	if ( (payload[0] & 0xf0) == 0x60 ){
-//  		return PROTOCOL_IPV6;
-// 	}
+	/* detect IPv6 */
+	if ( (payload[0] & 0xf0) == 0x60 ){
+		return PROTOCOL_IPV6;
+	}
 
 	return PROTOCOL_DATA;
 }
 
 static void gtp_format(FILE* fp, const struct header_chunk* header, const char* ptr, unsigned int flags){
-	const union gtp_header gtp = {.val = ntohl(*(const uint32_t*)ptr)};
+	const struct gtp_header* gtp = (const struct gtp_header*)ptr; 
 	fprintf(fp, ": GTP(verison: %d, message type: %02x. length: %d)",
-		gtp.version, gtp.message, gtp.total);
+		gtp->version, gtp->message, gtp->total);
 }
 
 static void gtp_dump(FILE* fp, const struct header_chunk* header, const char* ptr, const char* prefix, int flags){
-	const union gtp_header gtp = {.val = ntohl(*(const uint32_t*)ptr)};
+	const struct gtp_header* gtp = (const struct gtp_header*)ptr; 
 	char test = *ptr; 
 	char test1 = *(ptr+1); 
 	char test2 = *(ptr+2);
 	char test3 = *(ptr+3);
-	const char* payload = ptr + sizeof(union gtp_header);
+	const char* payload = ptr + sizeof(struct gtp_header);
 	fprintf(fp,"GTPoutput%0x %0x %0x %0x	\n", test, test1, test2, test3 );
-	fprintf(fp, "%sversion:              %0x\n", prefix, gtp.version);
-	fprintf(fp, "%message type:          %0X\n", prefix, gtp.message);
-	fprintf(fp, "%slength:               %d\n", prefix, gtp.total);
-	fprintf(fp, "%steid:                 0x%08x\n", prefix, gtp.teid);
+	fprintf(fp, "%sversion:              %0x\n", prefix, gtp->version);
+	fprintf(fp, "%message type:          %0x\n", prefix, gtp->message);
+	fprintf(fp, "%slength:               %d\n", prefix, gtp->total);
+	fprintf(fp, "%steid:                 0x%08x\n", prefix, gtp->teid);
 }
 
 
@@ -117,7 +110,7 @@ static void gtp_dump(FILE* fp, const struct header_chunk* header, const char* pt
 
 struct caputils_protocol protocol_gtp = {
 	.name = "GTP",
-	//.size = sizeof(uint32_t),
+	.size = sizeof(uint32_t),
 	.next_payload = gtp_header_next_payload,
 	.format = gtp_format,
 	.dump = gtp_dump,
